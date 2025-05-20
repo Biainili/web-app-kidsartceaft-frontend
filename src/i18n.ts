37f45bb679f9816aa0386af9fbdf36f15b353078
Biainili <barkhudaryan005@gmail.com>
2025-05-20 15:44:49 +0300

    feat: finish SEO pre-deploy (Helmet, hreflang, sitemap)

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,18 +2,22 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-import en from "./locales/en/translation.json";
-import ru from "./locales/ru/translation.json";
-import hy from "./locales/hy/translation.json";
+import enUI from "./locales/en/translation.json";
+import ruUI from "./locales/ru/translation.json";
+import hyUI from "./locales/hy/translation.json";
+
+import enSEO from "./locales/en/seo.json";
+import ruSEO from "./locales/ru/seo.json";
+import hySEO from "./locales/hy/seo.json";
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
-      en: { translation: en },
-      ru: { translation: ru },
-      hy: { translation: hy },
+      en: { translation: enUI, seo: enSEO },
+      ru: { translation: ruUI, seo: ruSEO },
+      hy: { translation: hyUI, seo: hySEO }
     },
     fallbackLng: "en",
     detection: {
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -4,6 +4,8 @@ import OrderImg from "../assets/img/order-img.png";
 import { Link, useParams } from "react-router-dom";
 import { MyOrders } from "../components/order-commponents/MyOrders";
 import { useTranslation } from "react-i18next";
+import { SEO } from "../components/SEO";
+import { getUrls } from "../utils/seo";
 
 export const ProfilePage: React.FC = () => {
   const { user, loading, fetchUserData } = useUser();
@@ -11,6 +13,7 @@ export const ProfilePage: React.FC = () => {
   const isFirstRender = useRef(true);
   const { t } = useTranslation();
   const { lang = "en" } = useParams();
+  const urls = getUrls("");
   const [formData, setFormData] = useState({
     username: user?.username || "",
     phone: user?.phone || "",
@@ -106,148 +109,167 @@ export const ProfilePage: React.FC = () => {
   if (!user) return <div>{t("profilePage.userNotFound")}</div>;
 
   return (
-    <div className="main-profile">
-      <div className="order-container-profile">
-        <h2 className="order-title">{t("profilePage.orderTitle")}</h2>
+    <>
+      <SEO
+        title={t("seo:profile.title")}
+        description={t("seo:profile.desc")}
+        canonical={urls[lang as "en" | "ru" | "hy"]}
+        alternates={[
+          { lang: "en", url: urls.en },
+          { lang: "ru", url: urls.ru },
+          { lang: "hy", url: urls.hy },
+        ]}
+      />
+      <div className="main-profile">
+        <div className="order-container-profile">
+          <h2 className="order-title">{t("profilePage.orderTitle")}</h2>
 
-        <div className="order-card">
-          {/* Фото детского рисунка */}
-          <img src={OrderImg} alt="Kids Drawing" className="order-image" />
+          <div className="order-card">
+            {/* Фото детского рисунка */}
+            <img src={OrderImg} alt="Kids Drawing" className="order-image" />
 
-          {/* Описание */}
-          <div className="order-details">
-            <p className="order-description">
-              {t("profilePage.orderDescription")}
-            </p>
+            {/* Описание */}
+            <div className="order-details">
+              <p className="order-description">
+                {t("profilePage.orderDescription")}
+              </p>
 
-            {/* Кнопка перехода на страницу заказа */}
-            <Link to={`/${lang}/order`} className="order-button">
-              {t("profilePage.orderButton")}
-            </Link>
-          </div>
-        </div>
-      </div>
-      <div className="containerProfileOrder">
-        <div className="profile-container">
-          <h2 className="profile-title">👤 {t("profilePage.title")}</h2>
-
-          <div className="profile-info">
-            <p>
-              <strong>{t("registerPage.usernameLabel")}:</strong>{" "}
-              {user.username}
-            </p>
-            <p>
-              <strong>{t("registerPage.emailLabel")}:</strong> {user.email}
-            </p>
-            <p>
-              <strong>{t("registerPage.phoneLabel")}:</strong> {user.phone}
-            </p>
-            <p>
-              <strong>{t("registerPage.locationLabel")}:</strong>{" "}
-              {user.location}
-            </p>
-          </div>
-
-          <button className="edit-btn" onClick={() => setIsEditing(!isEditing)}>
-            {isEditing ? t("profilePage.cancel") : t("profilePage.edit")}
-          </button>
-
-          {isEditing && (
-            <div className="edit-container">
-              <label>
-                {t("registerPage.usernameLabel")}:
-                <input
-                  type="text"
-                  name="username"
-                  value={formData.username}
-                  onChange={handleChange}
-                />
-              </label>
-
-              <label>
-                {t("registerPage.phoneLabel")}:
-                <input
-                  type="text"
-                  name="phone"
-                  value={formData.phone}
-                  onChange={handleChange}
-                />
-              </label>
-              <label>
-                {t("registerPage.locationLabel")}:
-                <input
-                  type="text"
-                  name="location"
-                  value={formData.location}
-                  onChange={handleChange}
-                />
-              </label>
-
-              <label>
-                {t("profilePage.currentPasswordLabel")}:
-                <div className="password-container">
-                  <input
-                    type={showPasswords.currentPassword ? "text" : "password"}
-                    name="currentPassword"
-                    value={formData.currentPassword}
-                    onChange={handleChange}
-                    placeholder={t("profilePage.currentPasswordLabel")}
-                  />
-                  <span
-                    onClick={() => togglePasswordVisibility("currentPassword")}
-                    className="eye-icon"
-                  >
-                    {showPasswords.currentPassword ? "👁" : "🙈"}
-                  </span>
-                </div>
-              </label>
-
-              <label>
-                {t("profilePage.newPasswordLabel")}:
-                <div className="password-container">
-                  <input
-                    type={showPasswords.newPassword ? "text" : "password"}
-                    name="newPassword"
-                    value={formData.newPassword}
-                    onChange={handleChange}
-                    placeholder={t("profilePage.newPasswordLabel")}
-                  />
-                  <span
-                    onClick={() => togglePasswordVisibility("newPassword")}
-                    className="eye-icon"
-                  >
-                    {showPasswords.newPassword ? "👁" : "🙈"}
-                  </span>
-                </div>
-              </label>
-
-              <label>
-                {t("profilePage.confirmNewPasswordLabel")}:
-                <div className="password-container">
-                  <input
-                    type={showPasswords.confirmPassword ? "text" : "password"}
-                    name="confirmPassword"
-                    value={formData.confirmPassword}
-                    onChange={handleChange}
-                    placeholder={t("profilePage.confirmNewPasswordLabel")}
-                  />
-                  <span
-                    onClick={() => togglePasswordVisibility("confirmPassword")}
-                    className="eye-icon"
-                  >
-                    {showPasswords.confirmPassword ? "👁" : "🙈"}
-                  </span>
-                </div>
-              </label>
-
-              <button className="save-btn" onClick={handleSave}>
-                {t("profilePage.saveChanges")}
-              </button>
+              {/* Кнопка перехода на страницу заказа */}
+              <Link to={`/${lang}/order`} className="order-button">
+                {t("profilePage.orderButton")}
+              </Link>
             </div>
-          )}
+          </div>
         </div>
-        <MyOrders userId={user.id} />
+        <div className="containerProfileOrder">
+          <div className="profile-container">
+            <h2 className="profile-title">👤 {t("profilePage.title")}</h2>
+
+            <div className="profile-info">
+              <p>
+                <strong>{t("registerPage.usernameLabel")}:</strong>{" "}
+                {user.username}
+              </p>
+              <p>
+                <strong>{t("registerPage.emailLabel")}:</strong> {user.email}
+              </p>
+              <p>
+                <strong>{t("registerPage.phoneLabel")}:</strong> {user.phone}
+              </p>
+              <p>
+                <strong>{t("registerPage.locationLabel")}:</strong>{" "}
+                {user.location}
+              </p>
+            </div>
+
+            <button
+              className="edit-btn"
+              onClick={() => setIsEditing(!isEditing)}
+            >
+              {isEditing ? t("profilePage.cancel") : t("profilePage.edit")}
+            </button>
+
+            {isEditing && (
+              <div className="edit-container">
+                <label>
+                  {t("registerPage.usernameLabel")}:
+                  <input
+                    type="text"
+                    name="username"
+                    value={formData.username}
+                    onChange={handleChange}
+                  />
+                </label>
+
+                <label>
+                  {t("registerPage.phoneLabel")}:
+                  <input
+                    type="text"
+                    name="phone"
+                    value={formData.phone}
+                    onChange={handleChange}
+                  />
+                </label>
+                <label>
+                  {t("registerPage.locationLabel")}:
+                  <input
+                    type="text"
+                    name="location"
+                    value={formData.location}
+                    onChange={handleChange}
+                  />
+                </label>
+
+                <label>
+                  {t("profilePage.currentPasswordLabel")}:
+                  <div className="password-container">
+                    <input
+                      type={showPasswords.currentPassword ? "text" : "password"}
+                      name="currentPassword"
+                      value={formData.currentPassword}
+                      onChange={handleChange}
+                      placeholder={t("profilePage.currentPasswordLabel")}
+                    />
+                    <span
+                      onClick={() =>
+                        togglePasswordVisibility("currentPassword")
+                      }
+                      className="eye-icon"
+                    >
+                      {showPasswords.currentPassword ? "👁" : "🙈"}
+                    </span>
+                  </div>
+                </label>
+
+                <label>
+                  {t("profilePage.newPasswordLabel")}:
+                  <div className="password-container">
+                    <input
+                      type={showPasswords.newPassword ? "text" : "password"}
+                      name="newPassword"
+                      value={formData.newPassword}
+                      onChange={handleChange}
+                      placeholder={t("profilePage.newPasswordLabel")}
+                    />
+                    <span
+                      onClick={() => togglePasswordVisibility("newPassword")}
+                      className="eye-icon"
+                    >
+                      {showPasswords.newPassword ? "👁" : "🙈"}
+                    </span>
+                  </div>
+                </label>
+
+                <label>
+                  {t("profilePage.confirmNewPasswordLabel")}:
+                  <div className="password-container">
+                    <input
+                      type={showPasswords.confirmPassword ? "text" : "password"}
+                      name="confirmPassword"
+                      value={formData.confirmPassword}
+                      onChange={handleChange}
+                      placeholder={t("profilePage.confirmNewPasswordLabel")}
+                    />
+                    <span
+                      onClick={() =>
+                        togglePasswordVisibility("confirmPassword")
+                      }
+                      className="eye-icon"
+                    >
+                      {showPasswords.confirmPassword ? "👁" : "🙈"}
+                    </span>
+                  </div>
+                </label>
+
+                <button className="save-btn" onClick={handleSave}>
+                  {t("profilePage.saveChanges")}
+                </button>
+              </div>
+            )}
+          </div>
+          <MyOrders userId={user.id} />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
